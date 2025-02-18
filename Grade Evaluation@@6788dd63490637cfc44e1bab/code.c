f125af1458221aff1a4f7ea40c2183d78568adec
lazyAkshit <akshit1464.becse24@chitkara.edu.in>
2025-02-18 12:57:24 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main{
+    char ch;
+    scanf("%c",&ch);
+    switch(ch){
+        case A:
+        printf("Excellent");
+        break;
+        case B:
+        printf("Good");
+        break;
+        case C:
+        printf("Average");
+        break;
+        case D:
+        printf("Below Average");
+        break;
+        case F:
+        printf("Fail");
+        break;
+    }
+    return 0;
+}
